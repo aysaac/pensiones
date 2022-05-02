5c945f749bbfe2984170eedc141bbf78a3332709
aysaac <isaacgr2014@hotmail.com>
2022-05-01 20:31:25 -0500

    Added Monto comulativo

--- isaac.R
+++ isaac.R
@@ -1,5 +1,6 @@
 library(lifecontingencies)
 library(rlist)
+#la incapacidad se considera 15%
 get_life_table<- function(tabla){
   if (tabla=="m"){
     table <- read_excel("Datos.xlsx", sheet = "Mlx")
@@ -20,11 +21,9 @@ get_life_table<- function(tabla){
   return(lifet)
 }
 
-sobrevivencia_conjuge <- function(fecha_hoy,
-                                  semanas_cotizadas,
+invalidez_conjuge <- function(fecha_hoy,
                                   carrera_salarial,
                                   datos,
-                                  INPC,
                                   UDIs,
                                   tasas) {
   tasa_seguridad_social=.035
@@ -56,17 +55,20 @@ sobrevivencia_conjuge <- function(fecha_hoy,
 
   edades_conyuge=(0:(110-edad_conyuge+1))
   probs_conyuge=pxt(table_conyuge,edad_conyuge,edades_conyuge)
-  pxt(table_conyuge,edad_conyuge,50)
+  # pxt(table_conyuge,edad_conyuge,50)
   convs_conyugue=probs_conyuge*b1+(1-probs_conyuge)*b2
   edades_asegurado=(0:(110-edad_asegurado))
   prob_asegurado=pxt(tabla_asegurado,edad_asegurado,edades_asegurado)
   tasas_de_asegurado=(1+tasa_seguridad_social)^(-edades_asegurado)
   convs_asegurado=prob_asegurado*tasas_de_asegurado
-  if(convs_conyugue>convs_asegurado){
-    dif=length(convs_conyugue)-length(convs_asegurado)
-    dif
+  if(length(convs_conyugue)>length(convs_asegurado)){
+
     convs_asegurado[(length(convs_asegurado)+1):length(convs_conyugue)]=0
-    convs_asegurado
+
+
+  }else if(length(convs_conyugue)<length(convs_asegurado)){
+
+    convs_conyugue[(length(convs_conyugue)+1):length(convs_asegurado)]=0
 
   }
   fecha_hoy
@@ -84,5 +86,91 @@ sobrevivencia_conjuge <- function(fecha_hoy,
   PNSI=anualidad*sum(convs_asegurado*convs_conyugue)
 
   FACBI=inicio_mes/inicio_ano
-  return(PNSI*FACBI)
+  return(PNSI*FACBI*1.03)#de comisones y adquisicion se asumen 3% extra
+}
+supervivencia_conjuge <- function(fecha_hoy,
+                                  carrera_salarial,
+                                  datos,
+                                  UDIs
+                                  ) {
+  tasa_seguridad_social=.035
+  # edades_asegurado=(0:(110-edad_asegurado))
+
+  carrera_salarial=as.data.frame(carrera_salarial)
+  carrera_salarial_ajustada=carrera_salarial$Salario_diario*
+    (tasa_ajuste_salario$rate[0:length(carrera_salarial$Salario_diario)])
+
+  tabla_asegurado=get_life_table('inv')
+  cat('tabla conyuge')
+  table_conyuge=get_life_table(datos$Sexo[2])
+  cat('tabla conyuge')
+  #CALCULO b1 #
+  Sal_promedio = sum(carrera_salarial_ajustada)/10
+  CB_IV = .35 * Sal_promedio
+  AF = .15
+  AA = .135
+  PGM = 107.8878642 #pension en el 2022
+  b1=.9
+  CB_IV_AA_AF = CB_IV * (1 + AA + AF)
+  rvda = CB_IV_AA_AF*b1
+  edad_conyuge=as.integer(datos[2,1])
+  edad_asegurado=as.integer(datos[1,1])
+  edades_asegurado=(0:(110-edad_asegurado))
+  tasas_de_asegurado=(1+tasa_seguridad_social)^(-edades_asegurado)
+  edades_conyuge=(0:(110-edad_conyuge+1))
+  convs_conyugue=pxt(table_conyuge,edad_conyuge,edades_conyuge)
+  probs_asegurado=pxt(tabla_asegurado,edad_conyuge,edades_asegurado)
+  convs_asegurado=(1-probs_asegurado)*tasas_de_asegurado
+  if(length(convs_conyugue)>length(convs_asegurado)){
+
+    convs_asegurado[(length(convs_asegurado)+1):length(convs_conyugue)]=0
+
+
+  }else if(length(convs_conyugue)<length(convs_asegurado)){
+
+    convs_conyugue[(length(convs_conyugue)+1):length(convs_asegurado)]=0
+
+  }
+  Suma_basica=sum(convs_asegurado*convs_conyugue)*b1*13
+  maximo_externo = max(CB_IV, PGM)
+  if (datos$Sexo[2]=='m' | rvda <=PGM ){
+    Suma_final=Suma_basica*1.15
+  }else{
+   Suma_final=Suma_basica
+  }
+
+
+
+
+  date=as.POSIXlt(fecha_hoy)
+  date$mday=1
+
+
+  index=which(UDIs$Fecha==(date))
+  inicio_mes=UDIs$SP68257[index]
+  inicio_mes
+
+
+  inicio_ano=7.108233
+
+
+  FACBI=inicio_mes/inicio_ano
+  return(maximo_externo*Suma_final*FACBI*(1+.02))#solo se toma en cuenta margen
+}
+monto_comulativo<-function (fecha_hoy,
+                      carrera_salarial,
+                      datos,
+                      UDIs
+                      ) {
+  supervivencia=supervivencia_conjuge(fecha_hoy,
+                                carrera_salarial,
+                                datos,
+                                UDIs
+          )
+  invalidez=invalidez_conjuge(fecha_hoy,
+                              carrera_salarial,
+                              datos,
+                              UDIs,
+                              tasas)
+  return(supervivencia+invalidez)
 }
